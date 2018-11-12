7036c1bf855a4a3ad938b3c9dd8d06219a0b6e6a
jumblesale <rob@robsollars.co.uk>
2018-11-12 19:45:06 +0000

    added config switch based on env variable

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from unbabel.config import dev_config
+import os
 
-app = dev_config()
+from unbabel.config import dev_config, test_config
+
+config = {
+    'test': test_config,
+    'dev':  dev_config,
+}[os.environ.get('ENVIRONMENT', 'test')]
+
+app = config()
 
 if __name__ == '__main__':
     app.run()
